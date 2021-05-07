a18b8b3129fb6e6ad8930857a5c4b48988065450
edgarleos <edgarleos@10.30.131.201>
2021-05-07 16:22:10 -0700

    Test
    Added both testclasses

--- CST438Assingment2/src/test/java/cst438/controllers/CityServiceTest.java
+++ CST438Assingment2/src/test/java/cst438/controllers/CityServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,7 +18,9 @@ import cst438.domain.City;
 import cst438.domain.CityInfo;
 import cst438.domain.CityRepository;
 import cst438.domain.Country;
+import cst438.domain.CountryRepository;
 import cst438.service.CityService;
+import cst438.service.WeatherService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,20 +29,25 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 
 
-@WebMvcTest(CityRestController.class)
-public class CityRestControllerTest {
+@WebMvcTest(CityServiceTest.class)
+public class CityServiceTest {
 	
 	@MockBean
-	private CityService CityService;
+	private CountryRepository countryRepository;
 	
 	@MockBean
 	private CityRepository cityRepository;
 	
 	
+	@MockBean
+	private WeatherService weatherService;
+	
+	@MockBean
+	private CityService CityService;
 	@Autowired
 	private MockMvc mvc;
 	
-	private JacksonTester<CityInfo> jsonCityAttempt;
+	private JacksonTester<City> jsonCityAttempt;
 	
 	@BeforeEach
 	public void setUPEach() {
@@ -49,26 +57,31 @@ public class CityRestControllerTest {
 
 	@Test
 	public void test1() throws Exception{
-		
-		Country country = new Country("TST", "Test Country");
-		City city = new City(1, "TestCity1", "DistrictTest", 100000, country);
-		List<City> cities = new ArrayList<City>();
-		cities.add(city);
-		
-		given(cityRepository.findByName("TestCity1")).willReturn(cities);
-		
-		CityInfo city1 = new CityInfo(1, "TestCity1", 50, "80.0 °F", "05.59 PM", "USA,","United States", "DistrictTest");
-				
-		given(CityService.getCityInfo("TestCity1")).willReturn(new CityInfo(1, "TestCity1", 50, "80.0 °F", "05.59 PM", "USA,","United States", "DistrictTest"));
-		MockHttpServletResponse response = mvc.perform(get("/cities/TestCity1")).andReturn().getResponse();
-		
-		CityInfo cityResult = jsonCityAttempt.parseObject(response.getContentAsString());
-		
-		CityInfo expectedResult = new CityInfo(1, "TestCity1", 50, "80.0 °F", "05.59 PM", "USA,","United States", "DistrictTest");
-		
-		expectedResult.setTemp( "80.0 °F");
-		
-		assertThat(cityResult).isEqualTo(expectedResult);
-	}
 
+	
+	Country country = new Country("TST", "Test Country");
+	City city = new City(1, "TestCity", "DistrictTest", 100000, country);
+	List<City> cities = new ArrayList<City>();
+	cities.add(city);
+
+		
+	given(cityRepository.findByName("TestCity")).willReturn(cities);
+
+
+	MockHttpServletResponse response = mvc.perform(get("/city/TestCity")).andReturn().getResponse();
+			
+
+
+	assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+	
+
+	City cityResult = jsonCityAttempt.parseObject(response.getContentAsString());
+	
+	City expectedResult = new  City(1, "TestCity", "DistrictTest", 100000, country);
+
+	assertThat(cityResult).isEqualTo(expectedResult);
+	}
+	
 }
+
+
