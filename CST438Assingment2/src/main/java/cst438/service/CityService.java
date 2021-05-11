5936a4f4cd2fb7498e7f7180215d66c52749164e
TonyV <TonyV@DESKTOP-RJIK1AP.lan>
2021-05-11 14:51:38 -0700

    Great work Edgar. I left a comment on the City Service file But no recommendations necessary this Code runs and produces desired output.

--- CST438Assingment2/src/main/java/cst438/service/CityService.java
+++ CST438Assingment2/src/main/java/cst438/service/CityService.java
@@ -19,16 +19,18 @@ public class CityService {
 		@Autowired
 		private WeatherService weatherService;
 		
+	
+		
 		public CityInfo getCityInfo(String cityName) {
 			List<City> cities = cityRepository.findByName(cityName);
 			City city = cities.get(0);
 			TimeAndTemp cityWeather = weatherService.getTempAndTime(cityName);
 			System.out.print(cityWeather.getTime()+cityWeather.getTimezone()*1000);
-			double tempF = Math.round((cityWeather.getTemp() - 273.15)* 9.0/5.0 +32.0);
+			double tempF = Math.round((cityWeather.getTemp() - 273.15) * 9.0/5.0 +32.0);
 			cityWeather.setTemp(tempF);
 			Date t = new Date();
 			long localTime = t.getTime();
-			long localOff= t.getTimezoneOffset()*60000;
+			long localOff= t.getTimezoneOffset()*60000; // why is this crossed out?
 			long utc = localTime + localOff;
 			long cityOff = utc+(1000*cityWeather.getTimezone());
 			DateFormat dateFormat = new SimpleDateFormat("hh.mm aa");
